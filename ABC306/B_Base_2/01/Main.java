8bbed2ee8aa6870c32d12622aa130ad2c87c5cf2
Lucina <lucina.tech@gmail.com>
2023-06-26 21:30:53 +0900

    ABC307 A Weekly Records 01

--- ABC306/B_Base_2/01/Main.java
+++ ABC306/B_Base_2/01/Main.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        // ===== 入力値の受け取り ===== 
         Scanner scanner = new Scanner(System.in);
         var str = scanner.nextLine().split(" ");
         scanner.close();
 
+        // ===== 指数計算 ===== 
         BigInteger result = BigInteger.ZERO;
         for (var i = 0; i < str.length; i++) {
             result = result.add(BigInteger.valueOf(Integer.valueOf(str[i])).multiply(BigInteger.TWO.pow(i)));
         }
+
+        // ===== 出力 ===== 
         System.out.println(result);
     }
 }
