cf4a219c3c2edbf16c2a791f061da166c9a0434e
Lucina <lucina.tech@gmail.com>
2023-07-01 14:29:09 +0900

    ABC307 A Weekly Records 02

--- ABC307/A_Weekly_Records/02/Main.java
+++ ABC307/A_Weekly_Records/02/Main.java
@@ -4,54 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        final Integer WEEK_COUNT = 7;
+
         // ===== 入力値の読取 =====
         Scanner scanner = new Scanner(System.in);
-        var weekNum = scanner.nextInt();
-        scanner.nextLine(); // 読み捨て
-
-        var walkNumLine = scanner.nextLine();
-        
-        var walkSumMap = new HashMap<Integer, Integer>();
-        
-        for (var walkNumStr : walkNumLine.split(" ")) {
-            Integer key = i / 7; // 小数点以下切捨て
-            
-            Integer.valueOf(walkNumStr);
-
-            if (walkSumMap.containsKey(key) ) {
-                var sum = walkSumMap.get(key);
-                sum = sum + walkNum;
-                walkSumMap.replace(key, sum);
-            }
-            i++;
-
-        }
-        scanner.nextLine(); // 読み捨て
-
-
-        
-        while (scanner.hasNextInt()) {
-            walkNumList.add(scanner.nextInt());
+        var N = scanner.nextInt();
+        var walkCountList = new ArrayList<Integer>();
+        for (var i = 0; i < N * WEEK_COUNT; i++) {
+            walkCountList.add(scanner.nextInt());
         }
         scanner.close();
 
         // ===== 出力値の生成 =====
-        var walkSumMap = new HashMap<Integer, Integer>();
-        var i = 0;
-        for (var walkNum : walkNumList) {
-            
-            if (walkSumMap.containsKey(key) ) {
-                var sum = walkSumMap.get(key);
-                sum = sum + walkNum;
-                walkSumMap.replace(key, sum);
+        var walkCountSumList = new ArrayList<String>();
+        var j = 0;
+        var sum = 0;
+        for (var walkCount : walkCountList) {
+            sum = sum + walkCount;
+            if ((j + 1) % 7 == 0) {
+                walkCountSumList.add(String.valueOf(sum));
+                sum = 0;
             }
-            i++;
+            j++;
         }
 
-        // ===== 出力 =====
-        for (var walkSum : walkSumMap.values()) {
-            System.out.print(walkSum);
-            System.out.print(" ");
-        }
+        // ===== 結果出力 =====
+        System.out.println(String.join(" ", walkCountSumList));
     }
 }
