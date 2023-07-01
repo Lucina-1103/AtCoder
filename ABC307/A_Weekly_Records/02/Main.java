c074312f79799e781fb2e9c52b3880d7dc550ef6
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2023-07-01 14:29:50 +0900

    Feature/mod abc307 a02 (#3)
    ABC307 A Weekly Records 02

---
+++ ABC307/A_Weekly_Records/02/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final Integer WEEK_COUNT = 7;
+
+        // ===== 入力値の読取 =====
+        Scanner scanner = new Scanner(System.in);
+        var N = scanner.nextInt();
+        var walkCountList = new ArrayList<Integer>();
+        for (var i = 0; i < N * WEEK_COUNT; i++) {
+            walkCountList.add(scanner.nextInt());
+        }
+        scanner.close();
+
+        // ===== 出力値の生成 =====
+        var walkCountSumList = new ArrayList<String>();
+        var j = 0;
+        var sum = 0;
+        for (var walkCount : walkCountList) {
+            sum = sum + walkCount;
+            if ((j + 1) % 7 == 0) {
+                walkCountSumList.add(String.valueOf(sum));
+                sum = 0;
+            }
+            j++;
+        }
+
+        // ===== 結果出力 =====
+        System.out.println(String.join(" ", walkCountSumList));
+    }
+}
