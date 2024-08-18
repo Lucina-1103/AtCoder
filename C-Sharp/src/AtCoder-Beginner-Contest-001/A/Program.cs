d14dd36d621f2ea1ae55273bac7904250fe45de0
Lucina <lucina.tech@gmail.com>
2024-08-18 18:19:46 +0900

    add:C#のdocker環境とABC001～002に関するファイルを追加

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-001/A/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder
+{
+    class ABC001_A
+    {
+        static void Main(string[] args)
+        {
+            int h1 = int.Parse(Console.ReadLine());
+            int h2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(h1 - h2);
+        }
+    }
+}
