288b508e4ba8ad983d2046897b7ec0fa4e49968c
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-08-18 18:21:41 +0900

    add:C#のDocker関連ファイルとサンプルプログラムを追加 (#26)

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-002/A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split(' ');
+
+            int x = int.Parse(array[0]);
+            int y = int.Parse(array[1]);
+
+            Console.WriteLine(Math.Max(x, y));
+        }
+    }
+}
