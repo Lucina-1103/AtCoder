59c69dff2d6c3796ad95bc027771b7ad9bee83c1
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-08-25 22:41:53 +0900

    fix:不要なusing句を削除 (#27)

--- C-Sharp/src/AtCoder-Beginner-Contest-001/A/Program.cs
+++ C-Sharp/src/AtCoder-Beginner-Contest-001/A/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AtCoder
 {
-    class ABC001_A
+    class Program
     {
         static void Main(string[] args)
         {
