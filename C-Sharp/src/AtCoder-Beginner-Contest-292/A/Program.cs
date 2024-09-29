2b11897b9344c62d6163f3458114a6102b4b173a
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-09-29 23:01:46 +0900

    add:ABC292 A CAPS LOCKの回答を追加 (#30)

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-292/A/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AtCoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smallLetterStr = Console.ReadLine();
+
+            Console.WriteLine(smallLetterStr.ToUpper());
+        }
+    }
+}
