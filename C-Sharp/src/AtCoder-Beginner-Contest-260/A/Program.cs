c49b217b5e3926d8258b657e45bb7b217b7cf8dc
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-10-14 11:04:51 +0900

    add:abc260-A (#32)

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-260/A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+var str = Console.ReadLine()!.ToCharArray();;
+
+var dictionary = new Dictionary<char, int>();
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (dictionary.ContainsKey(str[i]))
+    {
+        dictionary[str[i]]++;
+    }
+    else
+    {
+        dictionary.Add(str[i], 1);
+    }
+}
+
+var onceFlg = false;
+foreach (char letter in dictionary.Keys)
+{
+    if (dictionary[letter] == 1)
+    {
+        Console.WriteLine(letter);
+        onceFlg = true;
+        break;
+    }
+}
+
+if (!onceFlg)
+{
+    Console.WriteLine(-1);
+}
