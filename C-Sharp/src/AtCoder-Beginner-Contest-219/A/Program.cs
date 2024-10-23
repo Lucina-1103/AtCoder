f70b57eedf67311673bccda5b699a4cd7405d24d
Lucina <lucina.tech@gmail.com>
2024-10-23 20:28:19 +0900

    add:ABC219-Aの回答を追加

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-219/A/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+var num = int.Parse(Console.ReadLine()!);
+
+string result = "expert";
+if (0  <= num && num < 40) result = (40 - num).ToString();
+if (40 <= num && num < 70) result = (70 - num).ToString();
+if (70 <= num && num < 90) result = (90 - num).ToString();
+
+Console.WriteLine(result);
