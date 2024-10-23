f03274d4618aca98721ce02893ffb64cd4d6a1e2
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-10-23 22:17:39 +0900

    add:ABC315-Aを追加 (#34)

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-315/A/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+var str = Console.ReadLine()!;
+
+str = str.Replace("a", "");
+str = str.Replace("i", "");
+str = str.Replace("u", "");
+str = str.Replace("e", "");
+str = str.Replace("o", "");
+
+Console.WriteLine(str);
