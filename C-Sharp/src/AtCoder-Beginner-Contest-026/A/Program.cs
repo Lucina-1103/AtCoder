54d63e227c256fd6ca2cb23d638c0d7f11e5ae8c
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-10-29 00:13:41 +0900

    add:C#のABC026 A問題の回答を追加 (#36)

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-026/A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+var num = int.Parse(Console.ReadLine()!);
+
+var maxNum = 0;
+for (var i = 0; i < num; i++)
+{
+    var result = (num - i) * i;
+    if (maxNum < result)
+    {
+        maxNum = result;
+    }
+}
+
+Console.WriteLine(maxNum);
