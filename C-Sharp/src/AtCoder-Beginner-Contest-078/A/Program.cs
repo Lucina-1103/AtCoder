0cf96156caa2aa0100cd127c2aacf363e1635da1
Lucina <55339226+Lucina-1103@users.noreply.github.com>
2024-12-08 00:02:03 +0900

    ABC078-Aの回答を追加 (#37)

---
+++ C-Sharp/src/AtCoder-Beginner-Contest-078/A/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+string[] s = Console.ReadLine().Split();
+
+var str1 = strAry[0];
+var str2 = strAry[1];
+
+var result = string.Compare(str1, str2);
+
+switch (result)
+{
+    case -1:
+        Console.WriteLine("<");
+        break;
+    case 0:
+        Console.WriteLine("=");
+        break;
+    case 1:
+        Console.WriteLine(">");
+        break;
+}
